31962232970f884704c33e4026c09b48e211948a
Gianni Cuccu <gianni.cuccu@gmail.com>
2018-08-31 02:04:52 +0200

    Add comments

--- src/components/map.js
+++ src/components/map.js
@@ -19,25 +19,29 @@ class Map extends React.Component {
 
 
  
-  componentDidMount(){  
+  componentDidMount(){
     
+    // ASYNC load google maps 
+    // 
     let ref = window.document.getElementsByTagName("script")[0];
     let script = window.document.createElement("script");
     script.src = 'https://maps.googleapis.com/maps/api/js?v=3.31&key=AIzaSyA40zamRM0fp_m0JQKUWQPpyz0X_gXiFUA&callback=initMap';
     script.async = true;
     ref.parentNode.insertBefore(script, ref);
-    script.onerror = ()=>{ alert('GOOGLE MAP NETWORK LOADING ERROR') }
+    script.onerror = ()=>{ alert('GOOGLE MAP NETWORK LOADING ERROR') } // HAndle google maps loading error
     
+    // callback init function called by google maps script
     let initMap = () => {
-
+      //create the map instance
       let mymap = new window.google.maps.Map(this.refs.map, {
         zoom: 8,
         center:{lat: 40.0420784, lng: 9.0921147},
       });
-
+      // save the map to component state
       this.setState({map: mymap})
     }
     
+    // reference the global initMap function to the local initMap function
     window.initMap = initMap;
 
   }
@@ -45,18 +49,18 @@ class Map extends React.Component {
 
 
 render(){
-    
+  // do nothing until google.maps  global object becomes available 
   if (window.google) {
  
     const { map } = this.state
     const { activeLocation, sidebarLocationClick, setOpenRightPanel, getOpenRightPanel } = this.props
     let Infowindow = new window.google.maps.InfoWindow();
-    let bounds = new window.google.maps.LatLngBounds();
- 
+    let bounds = new window.google.maps.LatLngBounds(); 
 
+
+    // function to populate and handle info windows popups
     let  populateInfoWindow = (marker, largeInfowindow) => {
-        
-          
+                 
             if (largeInfowindow.marker !== marker && !getOpenRightPanel() ) {
                 largeInfowindow.marker = marker;
 
@@ -68,16 +72,17 @@ render(){
                 button.type = 'button';
                 button.value = 'Show info...'
 
+                // attach event listener to "show info button"
                 button.addEventListener('click', ()=>{
-                this.props.setOpenRightPanel(true, marker.location)
+                this.props.setOpenRightPanel(true, marker.location) // open right panel and pass current location
 
                   }
                 )
                 
                 largeInfowindow.setContent(content);
-                
                 largeInfowindow.open(map, marker);
-
+                
+                // create close listeners
                 largeInfowindow.addListener('closeclick', () =>{
                   largeInfowindow.marker = null;
                   sidebarLocationClick({})
@@ -88,16 +93,18 @@ render(){
             }
           }
     
+    // remove all markers from map and create an empty marker Set
     [...this.markers].map(marker => marker.setMap(null))
     this.markers = new Set()
     
 
-
+    // locations listed in sidebar
     let vis = this.props.showingLocations
     
-
+    // create markers from locations listed in sidebar
     for (let i = 0; i < vis.length; i++) {
-            
+      
+      // Animate marker if matches active location
       let animation = null
           activeLocation.id === vis[i].id ? animation = window.google.maps.Animation.BOUNCE : animation = null;
 
@@ -114,28 +121,28 @@ render(){
         location: vis[i]
       });
 
+      // add marker position to map bounds
       bounds.extend(marker.position);
+      // add marker to markers Set
       this.markers.add(marker);
       
-
+      // Add event listener to marker
       marker.addListener('click', function() {
-                populateInfoWindow(this, Infowindow);
-                sidebarLocationClick(this.location)
-                setOpenRightPanel(false)
+                populateInfoWindow(this, Infowindow); // open infoWindow
+                sidebarLocationClick(this.location)   // Pass clicked location to parent component
+                setOpenRightPanel(false) // close right Panel
       });
 
     //activeLocation.location && map.panTo(activeLocation.location)
+
+    // Open the marker for the active location 
     let currentmarkref = [...this.markers].find(el => el.id === activeLocation.id)
-    
-    //populateInfoWindow(activeLocation)
     populateInfoWindow(currentmarkref,Infowindow)
 
-          }
+    }
 
-          //this.setState({markers: tempMarkers})
-          // console.log(map)
-          //console.log([...this.markers])
-
+       
+      //FIXME: Basic MAP ZOOM and fit bounds control -- 
       if ( map.zoom  && map.zoom >= 2  && [...this.markers].length > 1 && window.innerWidth > 600) 
           {map.fitBounds(bounds);}
     }
